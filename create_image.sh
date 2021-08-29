4f13f97a9b1a8a1be0946c88920e4d6fcf7339e5
Robert Trebula <r@hq.sk>
2021-08-29 17:13:05 +0200

    btrfs compression, final resize, cache apt packages

--- create_image.sh
+++ create_image.sh
@@ -4,8 +4,9 @@ DEBIAN_SUITE=bullseye
 IMAGE=/mnt/btrfs1/image_installer/$DEBIAN_SUITE.img
 DEBOOTSTRAP_PACKAGES=linux-image-amd64,acpid,sudo
 # https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-11.0.0-amd64-kde+nonfree.log
-FIRMWARE_PKGS=arm-trusted-firmware-tools dns323-firmware-tools firmware-ath9k-htc firmware-linux-free firmware-microbit-micropython firmware-microbit-micropython-doc firmware-tomu gnome-firmware grub-firmware-qemu hdmi2usb-fx2-firmware sigrok-firmware-fx2lafw ubertooth-firmware ubertooth-firmware-source amd64-microcode atmel-firmware bluez-firmware dahdi-firmware-nonfree firmware-amd-graphics firmware-atheros firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intel-sound firmware-intelwimax firmware-ipw2x00 firmware-ivtv firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-nonfree firmware-misc-nonfree firmware-myricom firmware-netronome firmware-netxen firmware-qcom-media firmware-qcom-soc firmware-qlogic firmware-realtek firmware-samsung firmware-siano firmware-sof-signed firmware-ti-connectivity firmware-zd1211 intel-microcode midisport-firmware
-IMAGE_SIZE_MB=10000
+FIRMWARE_PKGS="arm-trusted-firmware-tools dns323-firmware-tools firmware-ath9k-htc firmware-linux-free firmware-microbit-micropython firmware-microbit-micropython-doc firmware-tomu gnome-firmware grub-firmware-qemu hdmi2usb-fx2-firmware sigrok-firmware-fx2lafw ubertooth-firmware ubertooth-firmware-source amd64-microcode atmel-firmware bluez-firmware dahdi-firmware-nonfree firmware-amd-graphics firmware-atheros firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intel-sound firmware-intelwimax firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-nonfree firmware-misc-nonfree firmware-myricom firmware-netronome firmware-netxen firmware-qcom-media firmware-qcom-soc firmware-qlogic firmware-realtek firmware-samsung firmware-siano firmware-sof-signed firmware-ti-connectivity firmware-zd1211 intel-microcode midisport-firmware"
+OTHER_PKGS="btrfs-progs vim"
+IMAGE_SIZE_MB=15000
 MAIN_VOL_MOUNT_POINT=/mnt/installer
 DEB_CACHE=/mnt/btrfs1/image_installer/deb_cache
 DEBIAN_MIRROR=http://ftp.sk.debian.org/debian/
@@ -38,7 +39,7 @@ fi
 
 findmnt $LOOP_DEV >/dev/null
 if [ "$?" -ne 0 ]; then
-	mount -o compress=LZO "$LOOP_DEV" "$MAIN_VOL_MOUNT_POINT"
+	mount -o compress=zstd:1 "$LOOP_DEV" "$MAIN_VOL_MOUNT_POINT"
 else
 	echo "$LOOP_DEV already mounted"
 fi
@@ -56,13 +57,20 @@ cd $MAIN_VOL_MOUNT_POINT/@
 mount -t proc /proc proc/
 mount --rbind /sys sys/
 mount --rbind /dev dev/
+mkdir var/cache/apt/
+mount --rbind /var/cache/apt/ var/cache/apt/
 cp /etc/resolv.conf etc/resolv.conf
 
 chroot . env DEBIAN_FRONTEND=noninteractive apt -y install task-kde-desktop
-chroot . env DEBIAN_FRONTEND=noninteractive apt -y install "$FIRMWARE_PKGS"
+chroot . env DEBIAN_FRONTEND=noninteractive apt -y install $FIRMWARE_PKGS
+
+SIZE=$(btrfs filesystem usage -b .  | grep Free | grep min | cut -d: -f 3 | sed 's/)//' | xargs)
+btrfs filesystem resize  -$SIZE .
+# TODO resize the image file now
 
 # cleanup
 cd $MAIN_VOL_MOUNT_POINT/@
+umount var/cache/apt/
 umount proc
 umount sys
 umount dev
