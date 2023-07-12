dd1145b3195ff25b5b75ca3c2972f517081e49f9
Robert Trebula <r@hq.sk>
2023-07-12 10:01:11 +0200

    new gnome image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -294,7 +294,7 @@ export default {
   </main>
 
   <footer>
-    <span>Opinionated Debian Installer version 20230701a</span>
+    <span>Opinionated Debian Installer version 20230712a</span>
     <span>Installer &copy;2022-2023 <a href="https://github.com/r0b0/debian-installer">Robert T</a></span>
     <span>Banner &copy;2022 <a href="https://github.com/julietteTaka/Emerald">Juliette Taka</a></span>
   </footer>
