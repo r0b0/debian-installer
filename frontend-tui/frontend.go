710258c3483109cb183b38335378ccccec7ecd63
Robert Trebula <r@hq.sk>
2023-12-06 16:06:42 +0100

    tui: get options from back-end

--- frontend-tui/frontend.go
+++ frontend-tui/frontend.go
@@ -12,50 +12,56 @@ import (
 	"strconv"
 )
 
-type model struct {
-	disk          string
-	debianVersion string
-	username      string
-	userFullName  string
-	userPassword  string
-	rootPassword  string
-	luksPassword  string
-	hostname      string
-	timezone      string
-	enableSwap    string
-	swapSize      string
-}
-
 func LOG(l io.Writer, format string, args ...any) {
 	_, _ = l.Write([]byte(fmt.Sprintf(format+"\n", args...)))
 }
 
-func (m *model) startInstallation(log io.Writer) error {
-	post := url.Values{}
-	post.Set("DISK", m.disk)
-	post.Set("DEBIAN_VERSION", m.debianVersion)
-	post.Set("USERNAME", m.username)
-	post.Set("USER_FULL_NAME", m.userFullName)
-	post.Set("USER_PASSWORD", m.userPassword)
-	post.Set("ROOT_PASSWORD", m.userPassword)
-	post.Set("LUKS_PASSWORD", m.luksPassword)
-	post.Set("HOSTNAME", m.hostname)
-	post.Set("TIMEZONE", m.timezone)
-	post.Set("ENABLE_SWAP", m.enableSwap)
-	post.Set("SWAP_SIZE", m.swapSize)
-	client := http.Client{}
-	resp, err := client.PostForm("http://localhost:5000/install", post)
+var SwapOptions = map[string]int{
+	"None":      0,
+	"File":      1,
+	"Partition": 2,
+}
+
+type Model struct {
+	Disk          string `json:"DISK"`
+	DebianVersion string `json:"DEBIAN_VERSION"`
+	Username      string `json:"USERNAME"`
+	UserFullName  string `json:"USER_FULL_NAME"`
+	UserPassword  string `json:"USER_PASSWORD"`
+	RootPassword  string `json:"ROOT_PASSWORD"`
+	LuksPassword  string `json:"LUKS_PASSWORD"`
+	Hostname      string `json:"HOSTNAME"`
+	Timezone      string `json:"TIMEZONE"`
+	EnableSwap    string `json:"ENABLE_SWAP"`
+	SwapSize      string `json:"SWAP_SIZE"`
+}
+type LoginResp struct {
+	Environ  Model  `json:"environ"`
+	HasEfi   bool   `json:"has_efi"`
+	Hostname string `json:"hostname"`
+}
+
+func parseLoginJson(data io.Reader) (Model, error) {
+	var login LoginResp
+	err := json.NewDecoder(data).Decode(&login)
 	if err != nil {
-		LOG(log, "Error posting form: %v", err)
-		return err
+		return Model{}, err
 	}
-	LOG(log, "Post status: %s", resp.Status)
-	return nil
+	return login.Environ, nil
+}
+func loginToBackend() (Model, error) {
+	client := http.Client{}
+	res, err := client.Get("http://localhost:5000/login")
+	if err != nil {
+		return Model{}, err
+	}
+	defer res.Body.Close()
+	return parseLoginJson(res.Body)
 }
 
 type BlockDevice struct {
 	Path  string `json:"path"`
-	Model string `json:"model"`
+	Model string `json:"Model"`
 	Size  string `json:"size"`
 }
 type LsblkResp struct {
@@ -70,40 +76,32 @@ func parseLsblkJson(data io.Reader) (LsblkResp, error) {
 	}
 	return devices, nil
 }
-
-func getAvailableDrives(drives *[]string, dropDown *tview.DropDown, log io.Writer) error {
+func getAvailableDrives() ([]string, []string, error) {
 	client := http.Client{}
 	res, err := client.Get("http://localhost:5000/block_devices")
 	if err != nil {
-		LOG(log, "Error geting available drives: %v", err)
-		return err
+		return []string{}, []string{}, err
 	}
 	defer res.Body.Close()
 	devices, err := parseLsblkJson(res.Body)
 	if err != nil {
-		LOG(log, "Failed to parse JSON data: %v", err)
-		return err
+		return []string{}, []string{}, err
 	}
-	LOG(log, "Available devices: %v", devices)
-	clear(*drives)
+	var drives []string
 	var driveDescriptions []string
 	for _, device := range devices.Blockdevices {
-		*drives = append(*drives, device.Path)
+		drives = append(drives, device.Path)
 		driveDescription := fmt.Sprintf("%s %s (%s)", device.Path, device.Model, device.Size)
 		driveDescriptions = append(driveDescriptions, driveDescription)
 	}
-	LOG(log, "Drives: %v", drives)
-	LOG(log, "Drive descriptions: %v", driveDescriptions)
-	dropDown.SetOptions(driveDescriptions, nil)
-	return nil
+	return drives, driveDescriptions, nil
 }
 
-func processOutput(wsUrl string, log io.Writer) error {
+func processOutput(wsUrl string, log io.Writer) {
 	origin := "http://localhost/"
 	ws, err := websocket.Dial(wsUrl, "", origin)
 	if err != nil {
 		LOG(log, "Failed to connect to web socket: %v", err)
-		return err
 	}
 	go func() {
 		for {
@@ -114,14 +112,43 @@ func processOutput(wsUrl string, log io.Writer) error {
 			}
 		}
 	}()
+}
 
+func (m *Model) startInstallation(log io.Writer) error {
+	post := url.Values{}
+
+	post.Set("DISK", m.Disk)
+	post.Set("DEBIAN_VERSION", m.DebianVersion)
+	post.Set("USERNAME", m.Username)
+	post.Set("USER_FULL_NAME", m.UserFullName)
+	post.Set("USER_PASSWORD", m.UserPassword)
+	post.Set("ROOT_PASSWORD", m.UserPassword)
+	post.Set("LUKS_PASSWORD", m.LuksPassword)
+	post.Set("HOSTNAME", m.Hostname)
+	post.Set("TIMEZONE", m.Timezone)
+	post.Set("ENABLE_SWAP", m.EnableSwap)
+	post.Set("SWAP_SIZE", m.SwapSize)
+	client := http.Client{}
+	resp, err := client.PostForm("http://localhost:5000/install", post)
+	if err != nil {
+		LOG(log, "Error posting form: %v", err)
+		return err
+	}
+	LOG(log, "Post status: %s", resp.Status)
 	return nil
 }
 
 func main() {
-	var devices []string
+	devices, deviceNames, err := getAvailableDrives()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to get available drives from back-end: %v", err))
+	}
 
-	m := model{debianVersion: "bookworm"}  // TODO fetch this from GET /login response.environ
+	m, err := loginToBackend()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to get configuration from back-end: %v", err))
+	}
+
 	greenColour := tcell.NewRGBColor(0x08, 0x69, 0x6b)
 
 	app := tview.NewApplication()
@@ -132,39 +159,44 @@ func main() {
 		SetChangedFunc(func() {
 			app.Draw()
 		})
-	// logView.SetBackgroundColor(tcell.ColorGray)
 
+	var SwapOptionKeys []string
+	for k := range SwapOptions {
+		SwapOptionKeys = append(SwapOptionKeys, k)
+	}
 	form := tview.NewForm().
-		AddDropDown("Installation Target Device", nil, 0, nil).
-		AddPasswordField("Disk Encryption Passphrase", "", 0, '*', func(text string) {
-			m.luksPassword = text
+		AddDropDown("Installation Target Device", deviceNames, 0, func(_ string, optionIndex int) {
+			m.Disk = devices[optionIndex]
+		}).
+		AddPasswordField("Disk Encryption Passphrase", m.LuksPassword, 0, '*', func(text string) {
+			m.LuksPassword = text
 		}). // TODO second time
-		AddPasswordField("Root Password", "", 0, '*', func(text string) {
-			m.rootPassword = text
+		AddPasswordField("Root Password", m.RootPassword, 0, '*', func(text string) {
+			m.RootPassword = text
 		}).
-		AddInputField("Regular User Name", "", 0, nil, func(text string) {
-			m.username = text
+		AddInputField("Regular User Name", m.Username, 0, nil, func(text string) {
+			m.Username = text
 		}).
-		AddInputField("Full Name", "", 0, nil, func(text string) {
-			m.userFullName = text
+		AddInputField("Full Name", m.UserFullName, 0, nil, func(text string) {
+			m.UserFullName = text
 		}).
-		AddPasswordField("Regular User Password", "", 0, '*', func(text string) {
-			m.userPassword = text
+		AddPasswordField("Regular User Password", m.UserPassword, 0, '*', func(text string) {
+			m.UserPassword = text
 		}).
-		AddInputField("Hostname", "debian", 0, nil, func(text string) {
-			m.hostname = text
+		AddInputField("Hostname", m.Hostname, 0, nil, func(text string) {
+			m.Hostname = text
 		}).
-		AddInputField("Time Zone", "UTC", 0, nil, func(text string) {
-			m.timezone = text // TODO dropdown
+		AddInputField("Time Zone", m.Timezone, 0, nil, func(text string) {
+			m.Timezone = text // TODO dropdown
 		}).
-		AddDropDown("Enable Swap", []string{"None", "Partition", "File"}, 0, func(option string, optionIndex int) {
-			m.enableSwap = option
+		AddDropDown("Enable Swap", SwapOptionKeys, SwapOptions[m.EnableSwap], func(option string, optionIndex int) {
+			m.EnableSwap = option
 		}).
-		AddInputField("Swap Size", "1", 0, func(textToCheck string, lastChar rune) bool {
+		AddInputField("Swap Size", m.SwapSize, 0, func(textToCheck string, lastChar rune) bool {
 			_, err := strconv.Atoi(textToCheck)
 			return err == nil
 		}, func(text string) {
-			m.swapSize = text
+			m.SwapSize = text
 		}).
 		AddButton("Install OVERWRITING THE WHOLE DRIVE", func() {
 			err := m.startInstallation(logView)
@@ -172,29 +204,13 @@ func main() {
 				LOG(logView, "Failed to start installation: %v", err)
 			}
 		}).
-		// SetLabelColor(tcell.ColorWhite).
-		// SetFieldBackgroundColor(tcell.ColorGray).
-		// SetFieldTextColor(tcell.ColorBlack).
-		// SetButtonBackgroundColor(green_colour).
 		AddFormItem(logView)
 	form.SetBorder(true).
 		SetTitle("Opinionated Debian Installer").
 		SetTitleColor(greenColour).
 		SetTitleAlign(tview.AlignCenter)
 
-	// TODO fetch everything from the back-end first and create the form after - it will be much easier
-	_ = processOutput("ws://localhost:5000/process_output", logView)
-	devicesDropdown := form.GetFormItemByLabel("Installation Target Device").(*tview.DropDown)
-	go func() {
-		_ = getAvailableDrives(&devices, devicesDropdown, logView)
-		devicesDropdown.SetSelectedFunc(func(_ string, index int) {
-			if index < 0 {
-				return
-			}
-			m.disk = devices[index]
-			LOG(logView, "Selected device path '%s'", devices[index])
-		})
-	}()
+	processOutput("ws://localhost:5000/process_output", logView)
 
 	if err := app.SetRoot(form, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
--- frontend-tui/frontend_test.go
+++ frontend-tui/frontend_test.go
@@ -8,7 +8,7 @@ import (
 func TestParseLsblkJson(t *testing.T) {
 	f, err := os.Open("test_data/lsblk.json")
 	if err != nil {
-		panic(err)
+		t.Fatalf("Failed to open json file: %v", err)
 	}
 	devices, err := parseLsblkJson(f)
 	if err != nil {
@@ -17,10 +17,25 @@ func TestParseLsblkJson(t *testing.T) {
 	if len(devices.Blockdevices) == 0 {
 		t.Fatalf("No devices parsed: %v", devices.Blockdevices)
 	}
+
 	device := devices.Blockdevices[0]
 	t.Logf("First device: %v", device)
 	if "/dev/sda" != device.Path {
 		t.Errorf("First device path = %s; want /dev/sda", device.Path)
 	}
+}
 
+func TestParseLoginJson(t *testing.T) {
+	f, err := os.Open("test_data/login.json")
+	if err != nil {
+		t.Fatalf("Failed to open json file: %v", err)
+	}
+	login, err := parseLoginJson(f)
+	if err != nil {
+		t.Fatalf("Failed to parse json: %v", err)
+	}
+
+	if "/dev/sda" != login.Disk {
+		t.Errorf("Disk for installation = %s; want /dev/sda", login.Disk)
+	}
 }
